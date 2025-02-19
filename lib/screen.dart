186065bbd5becdf0ef4ded157aabfac996acde67
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2025-02-19 23:12:58 +0300

    screen updated

--- lib/screen.dart
+++ lib/screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_theme/theme/theme_notifier.dart';
+import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class Screen extends StatefulWidget {
@@ -32,13 +34,30 @@ class _ScreenState extends State<Screen> {
 
   @override
   Widget build(BuildContext context) {
+    final themeNotifier = Provider.of<ThemeNotifier>(context);
+
     return Scaffold(
       appBar: AppBar(
         elevation: 0,
-        backgroundColor: Colors.black,
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [Text("Theme App", style: TextStyle(color: Colors.white))],
+          children: [
+            Text(
+              "Theme App",
+              style: TextStyle(color: Theme.of(context).colorScheme.surface),
+            ),
+            Switch(
+              value: isDarkMode,
+              onChanged: (value) {
+                setState(() {
+                  isDarkMode = value;
+                  saveSwitchState();
+                  themeNotifier.toggleTheme();
+                });
+              },
+            ),
+          ],
         ),
       ),
     );
