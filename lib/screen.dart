fec68acd4b5af85e8db53350ed6dc705e9632db0
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2025-02-19 23:06:28 +0300

    screen updated

--- lib/screen.dart
+++ lib/screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class Screen extends StatefulWidget {
   const Screen({super.key});
@@ -9,6 +9,27 @@ class Screen extends StatefulWidget {
 }
 
 class _ScreenState extends State<Screen> {
+  bool isSwitched = false;
+  bool isDarkMode = false;
+
+  @override
+  void initState() {
+    super.initState();
+    loadSwitchState();
+  }
+
+  void loadSwitchState() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    setState(() {
+      isDarkMode = prefs.getBool('isDarkMode') ?? false;
+    });
+  }
+
+  void saveSwitchState() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setBool('isDarkMode', isDarkMode);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,10 +38,7 @@ class _ScreenState extends State<Screen> {
         backgroundColor: Colors.black,
         title: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Text("Theme App", style: TextStyle(color: Colors.white)),
-            FaIcon(FontAwesomeIcons.toggleOn, color: Colors.white),
-          ],
+          children: [Text("Theme App", style: TextStyle(color: Colors.white))],
         ),
       ),
     );
